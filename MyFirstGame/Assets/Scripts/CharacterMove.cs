aad451a8c937a796b25cd1e41b03ad6beaa9fe75
Youssef Ibrahim <youssefhossam12.yh@gmail.com>
2022-09-16 16:09:29 +0200

    1.0v hero game

---
+++ MyFirstGame/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    CharacterController Controller;
+    public float speed = 5;
+    float gravity = 10;
+    float verticalVelocity = 0;
+    public float jump = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontal, 0, vertical);
+
+        if (Controller.isGrounded)
+        {
+            if (Input.GetAxis("Jump") > 0)
+                verticalVelocity = jump;
+        }
+        else
+            verticalVelocity -= gravity * Time.deltaTime;
+        
+
+        if (moveDirection.magnitude > 0.1)
+        {
+            float angle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+        }
+
+        moveDirection = Camera.main.transform.TransformDirection(moveDirection);
+        moveDirection = new Vector3(moveDirection.x, verticalVelocity, moveDirection.z);
+
+        Controller.Move( moveDirection * Time.deltaTime * speed);
+    }
+}
